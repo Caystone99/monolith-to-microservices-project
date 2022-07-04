018135fabbf485f7aa5555ad140eddfcfd487628
Caystone99 <emmanuelcaleb546@gmail.com>
2022-07-04 05:45:56 +0100

    project_submit

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://af28aa6a4801b4c9c88dc66178ef8e4c-302834702.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
